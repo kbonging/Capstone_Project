c233debea8802fe1fd84e1cf840695408cac65a6
bongjung kim <apple75391@gmail.com>
2025-05-23 15:45:35 +0900

    소상공인 회원 가입 완료 -파일 추가 목록- OwnerController OwnerDAO OwnerDAOMapper.xml OwnerDTO OwnerService OwnerServiceImpl
    시큐리티 설정 수정
    커뮤니티부분 url 요청 모두 허용 [임시]
    
    - bongjung 2025-05-23 -

--- backend/src/main/webapp/DBsql/query_SQL.sql
+++ backend/src/main/webapp/DBsql/query_SQL.sql
@@ -1,39 +1,22 @@
-################# 이메일로 회원 조회 ####################
-SELECT 
-    m.MEMBER_IDX,
-	m.MEMBER_ID,
-	m.MEMBER_PWD,
-	m.MEMBER_NAME,
-	m.MEMBER_EMAIL,
-    m.MEMBER_NICKNAME,
-    m.PROFILE_IMAGE_URL,
-    ma.AUTH,
-	m.DEL_YN,
-	m.REG_DATE,
-	m.MOD_DATE
-FROM tb_member m join tb_member_auth ma
-on m.MEMBER_IDX=ma.MEMBER_IDX
-WHERE MEMBER_EMAIL='apple75391@gmail.com'AND DEL_YN = 'N';
+################# 리뷰어 전체 조회 ##################
+select * 
+FROM TB_MEMBER MB JOIN TB_MEMBER_AUTH AU
+ON MB.MEMBER_IDX = AU.MEMBER_IDX
+JOIN TB_REVIEWER_PROFILE RP
+ON MB.MEMBER_IDX = RP.MEMBER_IDX;
 
-################# 아이디로 회원 조회 ####################
-SELECT 
-    m.MEMBER_IDX,
-	m.MEMBER_ID,
-	m.MEMBER_PWD,
-	m.MEMBER_NAME,
-	m.MEMBER_EMAIL,
-    m.MEMBER_NICKNAME,
-    m.PROFILE_IMAGE_URL,
-    ma.AUTH,
-	m.DEL_YN,
-	m.REG_DATE,
-	m.MOD_DATE
-FROM tb_member m join tb_member_auth ma
-on m.MEMBER_IDX=ma.MEMBER_IDX
-WHERE MEMBER_ID='memtest05'AND DEL_YN = 'N';
+############ 리뷰어 채널 전체 조회 #############
+select MB.MEMBER_IDX, MB.MEMBER_NAME, RC.INF_TYPE_CODE_ID, RC.CHANNEL_URL
+FROM TB_MEMBER MB JOIN TB_MEMBER_AUTH AU
+ON MB.MEMBER_IDX = AU.MEMBER_IDX
+JOIN TB_REVIEWER_PROFILE RP
+ON MB.MEMBER_IDX = RP.MEMBER_IDX
+JOIN TB_REVIEWER_CHANNEL RC
+ON MB.MEMBER_IDX = RC.MEMBER_IDX;
+#################　소상공인　전체　조회 ####################
+select * 
+FROM TB_MEMBER MB JOIN TB_MEMBER_AUTH AU
+ON MB.MEMBER_IDX = AU.MEMBER_IDX
+JOIN TB_OWNER_PROFILE OP
+ON MB.MEMBER_IDX = OP.MEMBER_IDX;
 
-################ 아이디 유무 확인(중복 체크) ##############
-SELECT COUNT(*) FROM tb_member WHERE MEMBER_ID='admin';
-
-################ 이메일 유무 확인(중복 체크)#############
-SELECT COUNT(*) FROM tb_member WHERE MEMBER_EMAIL='apple75391@gmail.com';
