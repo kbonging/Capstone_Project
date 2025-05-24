0b6935db676330e8d1afb64159303140bd0d0b20
Bongjung Kim <apple75391@gmail.com>
2025-05-25 00:39:40 +0900

    쿼리 파일 수정 대문자 > 소문자
    - bongjung 2025-05-25 -

--- backend/src/main/webapp/DBsql/import_SQL.sql
+++ backend/src/main/webapp/DBsql/import_SQL.sql
@@ -6,151 +6,112 @@
 -- use capstone;
 -- drop database capstone;
 ######################### 회원 테이블 시작 ######################
-# 회원 테이블 삭제
+-- 회원 테이블 삭제
 -- drop table tb_member;
-# 회원 테이블 데이터만 삭제
+-- 회원 테이블 데이터만 삭제
 -- truncate table tb_member;
-# 공통 코드 테이블 데이터만 삭제
--- truncate table TB_COMMON_CODE;
+-- 공통 코드 테이블 데이터만 삭제
+-- truncate table tb_common_code;
 
-# truncate 실행 시 foreign_key가 등록된 테이블을 truncate할 경우 오류가 발생한다.
-# 아래와 같이 해결
--- set FOREIGN_KEY_CHECKS = 0;
+-- truncate 실행 시 foreign_key가 등록된 테이블을 truncate할 경우 오류가 발생한다.
+-- 아래와 같이 해결
+-- set foreign_key_checks = 0;
 
-
-
-# tb_member 테이블
--- CREATE TABLE tb_member (
---     MEMBER_IDX INT PRIMARY KEY AUTO_INCREMENT COMMENT '회원 고유번호', -- 회원 고유번호 (PK)
---     MEMBER_ID VARCHAR(60) UNIQUE COMMENT '로컬 회원 아이디', -- 중복안됨, NULL허용
---     MEMBER_PWD VARCHAR(255) COMMENT '로컬 회원 비밀번호', -- 로컬 회원 비밀번호 (소셜 로그인 계정은 비밀번호 없을 수 있음)
---     MEMBER_NAME VARCHAR(30) NOT NULL COMMENT '회원명',
---     MEMBER_EMAIL VARCHAR(255) UNIQUE NOT NULL COMMENT '회원 이메일 (로컬+소셜 공통)',
---     MEMBER_NICKNAME VARCHAR(60) COMMENT '닉네임',
---     PROFILE_IMAGE_URL VARCHAR(500) COMMENT '프로필 이미지 URL',
---     DEL_YN CHAR(1) NOT NULL DEFAULT 'N' COMMENT '삭제 여부(Y, N)',
---     REG_DATE DATETIME NOT NULL COMMENT '가입일',
---     MOD_DATE DATETIME COMMENT '수정일'
--- ) COMMENT='회원 테이블';
-
--- # 권한 테이블
--- CREATE TABLE TB_MEMBER_AUTH(
--- 	AUTH_IDX INT PRIMARY KEY AUTO_INCREMENT COMMENT '권한 고유번호',
---     MEMBER_IDX INT NOT NULL COMMENT '회원 고유번호',
---     AUTH VARCHAR(60) NOT NULL COMMENT '권한(ROLE_USER, ROLE_ADMIN)'
--- );
-
-# tb_social_member 테이블 (현재 사용 안함)
--- CREATE TABLE tb_social_member (
---     SOCIAL_IDX INT PRIMARY KEY AUTO_INCREMENT COMMENT '소셜 고유 번호', -- 소셜 고유번호 (PK)
---     MEMBER_IDX INT NOT NULL COMMENT '회원 테이블의 회원 고유번호 (FK)', -- tb_member의 MEMBER_IDX 참조
---     SOCIAL_TYPE VARCHAR(20) NOT NULL COMMENT '소셜 로그인 타입 (예: kakao, google)', -- 소셜 로그인 서비스 타입
---     SOCIAL_ID VARCHAR(255) UNIQUE NOT NULL COMMENT '소셜 로그인 사용자 고유 ID', -- 소셜 로그인 서비스에서 제공하는 사용자 ID
---     FOREIGN KEY (MEMBER_IDX) REFERENCES tb_member(MEMBER_IDX) ON DELETE CASCADE -- FK 설정 및 삭제 규칙
--- ) COMMENT='소셜 로그인 정보 테이블'; -- 테이블 설명
+-- 소셜 멤버 테이블 (현재 사용 안함)
+-- create table tb_social_member (
+--     social_idx int primary key auto_increment comment '소셜 고유 번호',
+--     member_idx int not null comment '회원 테이블의 회원 고유번호',
+--     social_type varchar(20) not null comment '소셜 로그인 타입 (예: kakao, google)',
+--     social_id varchar(255) unique not null comment '소셜 로그인 사용자 고유 id',
+--     foreign key (member_idx) references tb_member(member_idx) on delete cascade
+-- ) comment='소셜 로그인 정보 테이블';
 ######################### 회원 테이블 끝 #######################
 
-################## 새로운 DB 설계 중입니다 ###################
+################## 새로운 db 설계 중입니다 ###################
 ############ 아래 코드는 얘기 전 까지 import(실행) 금지 !! ############
 #########################################################
-##! 공통코드 !## (완)
-CREATE TABLE TB_COMMON_CODE (
-	CODE_ID		VARCHAR(20)		PRIMARY KEY NOT NULL COMMENT '코드번호', -- 예) INF001, INF002 등
-	GROUP_CODE	VARCHAR(20)		NULL				COMMENT '그룹 코드', -- 예) 인풀루언서 유형이면 INF_TYPE
-	CODE_NM		VARCHAR(100)	NOT NULL			COMMENT '코드이름',
-	IMAGE_URL	VARCHAR(255)	NULL				COMMENT '이미지 경로',
-    GROUP_SORT 	INT 			NULL	DEFAULT 0 	COMMENT '그룹 정렬 순서',
-	SORT		INT				NULL	DEFAULT 0	COMMENT '정렬 순서',
-	CODE_DC		VARCHAR(255)	NULL				COMMENT '코드 설명',
-	DEL_YN      CHAR(1)      	NOT NULL DEFAULT 'N'  COMMENT '삭제 여부(Y/N)',
-    REG_DATE    DATETIME     	NOT NULL             COMMENT '가입일시',
-    MOD_DATE    DATETIME                            COMMENT '수정일시'
-);
--- drop table TB_COMMON_CODE;
 
-##! 공통 회원 !## (완)
-CREATE TABLE TB_MEMBER (
-    MEMBER_IDX       INT            PRIMARY KEY AUTO_INCREMENT   COMMENT '회원 고유번호',
-    MEMBER_ID        VARCHAR(60)    UNIQUE NOT NULL 			 COMMENT '로그인 아이디',
-    MEMBER_PWD       VARCHAR(255)   NOT NULL                     COMMENT '비밀번호',
-    MEMBER_NAME      VARCHAR(30)    NOT NULL                     COMMENT '실명(이름) (한글만 입력)',
-    MEMBER_EMAIL     VARCHAR(255)   UNIQUE NOT NULL              COMMENT '이메일',
-    MEMBER_PHONE	 VARCHAR(20)    NULL                         COMMENT '휴대폰 번호',
-    PROFILE_IMG_URL VARCHAR(500)  DEFAULT 'defaultProfile.png'  COMMENT '프로필 이미지 URL (미작성시 기본 프로필 적용)',
-    INTRO		 	 TEXT	NULL								COMMENT '소개글',
-	HEART_COUNT		 INT	NULL	DEFAULT 0					COMMENT '하트수',
-	PENALTY			 INT	NULL	DEFAULT 0					COMMENT '패널티',
-    DEL_YN           CHAR(1)      NOT NULL DEFAULT 'N'          COMMENT '삭제 여부(Y/N)',
-    REG_DATE         DATETIME     NOT NULL                      COMMENT '가입일시',
-    MOD_DATE         DATETIME                                   COMMENT '수정일시'
+-- 공통코드
+create table tb_common_code (
+	code_id			varchar(20)		primary key not null comment '코드번호',
+	group_code		varchar(20)		null comment '그룹 코드',
+	code_nm			varchar(100)	not null comment '코드이름',
+	image_url		varchar(255)	null comment '이미지 경로',
+	group_sort		int 			null default 0 comment '그룹 정렬 순서',
+	sort			int			null default 0 comment '정렬 순서',
+	code_dc			varchar(255)	null comment '코드 설명',
+	del_yn			char(1)			not null default 'n' comment '삭제 여부(y/n)',
+	reg_date		datetime		not null comment '가입일시',
+	mod_date		datetime		null comment '수정일시'
+)comment='공통 코드 테이블';
+
+-- 공통 회원
+create table tb_member (
+	member_idx		int			primary key auto_increment comment '회원 고유번호',
+	member_id		varchar(60)	unique not null comment '로그인 아이디',
+	member_pwd		varchar(255)	not null comment '비밀번호',
+	member_name		varchar(30)	not null comment '실명(이름) (한글만 입력)',
+	member_email	varchar(255)	unique not null comment '이메일',
+	member_phone	varchar(20)	null comment '휴대폰 번호',
+	profile_img_url	varchar(500)	default 'defaultprofile.png' comment '프로필 이미지 url (미작성시 기본 프로필 적용)',
+	intro			text		null comment '소개글',
+	heart_count		int			null default 0 comment '하트수',
+	penalty			int			null default 0 comment '패널티',
+	del_yn			char(1)			not null default 'n' comment '삭제 여부(y/n)',
+	reg_date		datetime		not null comment '가입일시',
+	mod_date		datetime		null comment '수정일시'
 ) COMMENT='공통 회원 정보 테이블';
--- drop table TB_MEMBER;
 
-##! 권한 !## (완)
-CREATE TABLE TB_MEMBER_AUTH(
-	AUTH_IDX 	INT 		PRIMARY KEY AUTO_INCREMENT 	COMMENT '권한 고유번호',
-    MEMBER_IDX 	INT 		NOT NULL 					COMMENT '회원 고유번호',
-    AUTH 		VARCHAR(60) NOT NULL 					COMMENT '권한(ROLE_USER, ROLE_ADMIN, ROLE_OWNER) 리뷰어, 관리자, 소상공인',
-    FOREIGN KEY (MEMBER_IDX) REFERENCES TB_MEMBER(MEMBER_IDX) 
-		ON DELETE CASCADE  
-        ON UPDATE CASCADE
-) COMMENT='권한 테이블';
--- drop table TB_MEMBER_AUTH;
+-- 권한
+create table tb_member_auth (
+	auth_idx		int			primary key auto_increment comment '권한 고유번호',
+	member_idx		int			not null comment '회원 고유번호',
+	auth			varchar(60)	not null comment '권한(role_user, role_admin, role_owner)',
+	foreign key (member_idx) references tb_member(member_idx)
+		on delete cascade
+		on update cascade
+)comment='권한 테이블';
 
-##! 리뷰어프로필  !## (완)
-CREATE TABLE TB_REVIEWER_PROFILE (
-	MEMBER_IDX 		INT 			NOT NULL 	COMMENT '회원 고유번호',
-	NICKNAME 		VARCHAR(60) 	NULL 		COMMENT '회원 닉네임',
-	GENDER 			CHAR(1) 		NULL 		COMMENT '성별',
-	BIRTH_DATE		VARCHAR(10) 	NULL 		COMMENT '생년월일',
-	ACTIVITY_AREA 	VARCHAR(30) 	NULL 		COMMENT '활동지역',
-	ACTIVITY_TOPIC  VARCHAR(30) 	NULL 		COMMENT '활동주제 (공통코드 ACT_TOPIC)', -- 맛집, 식품 뷰티 등등
-	ZIP_CODE 		VARCHAR(10) 	NULL 		COMMENT '우편번호',
-	ADDRESS 		VARCHAR(255) 	NULL 		COMMENT '주소',
-	DETAIL_ADDRESS VARCHAR(255) 	NULL 		COMMENT '상세주소',
-	PRIMARY KEY (MEMBER_IDX),
-	FOREIGN KEY (MEMBER_IDX) REFERENCES TB_MEMBER(MEMBER_IDX)
-		ON DELETE CASCADE  
-        ON UPDATE CASCADE
+-- 리뷰어 프로필
+create table tb_reviewer_profile (
+	member_idx		int			not null comment '회원 고유번호',
+	nickname		varchar(60)	null comment '회원 닉네임',
+	gender			char(1)		null comment '성별',
+	birth_date		varchar(10)	null comment '생년월일',
+	activity_area	varchar(30)	null comment '활동지역',
+	activity_topic	varchar(30)	null comment '활동주제 (공통코드 act_topic)',
+	zip_code		varchar(10)	null comment '우편번호',
+	address			varchar(255)	null comment '주소',
+	detail_address	varchar(255)	null comment '상세주소',
+	primary key (member_idx),
+	foreign key (member_idx) references tb_member(member_idx)
+		on delete cascade
+		on update cascade
 ) COMMENT='리뷰어 프로필 테이블';
--- drop table TB_REVIEWER_PROFILE;
 
-##!  리뷰어 채널 정보 !## (완)
-CREATE TABLE TB_REVIEWER_CHANNEL (
-	REV_CHA_IDX 		INT PRIMARY KEY AUTO_INCREMENT 	COMMENT '채널 고유번호',
-	MEMBER_IDX 			INT 		NOT NULL 			COMMENT '회원 고유번호',
-	INF_TYPE_CODE_ID 	VARCHAR(20) NOT NULL 			COMMENT '인플루언서 유형 코드 (공통 코드 INF_TYPE)',
-	CHANNEL_URL 		VARCHAR(500) NOT NULL			COMMENT '채널 주소',
-	REG_DATE 			DATETIME 	NOT NULL 			COMMENT '등록일',
-	MOD_DATE 			DATETIME	 NULL 				COMMENT '수정일시',
-	FOREIGN KEY (MEMBER_IDX) REFERENCES TB_MEMBER(MEMBER_IDX)
-    	ON DELETE CASCADE
-        ON UPDATE CASCADE,
-	FOREIGN KEY (INF_TYPE_CODE_ID) REFERENCES TB_COMMON_CODE(CODE_ID)
-    	ON DELETE CASCADE
-        ON UPDATE CASCADE
+-- 리뷰어 채널 정보
+create table tb_reviewer_channel (
+	rev_cha_idx		int			primary key auto_increment comment '채널 고유번호',
+	member_idx			int		not null comment '회원 고유번호',
+	inf_type_code_id	varchar(20)	not null comment '인플루언서 유형 코드 (공통 코드 inf_type)',
+	channel_url			varchar(500)	not null comment '채널 주소',
+	reg_date			datetime	not null comment '등록일',
+	mod_date			datetime	null comment '수정일시',
+	foreign key (member_idx) references tb_member(member_idx)
+		on delete cascade
+		on update cascade,
+	foreign key (inf_type_code_id) references tb_common_code(code_id)
+		on delete cascade
+		on update cascade
 ) COMMENT='리뷰어 채널 정보(리뷰어=인플루언서 유형 정보)';
--- drop table TB_REVIEWER_CHANNEL;
 
-##! 소상공인 !## (완)
-CREATE TABLE TB_OWNER_PROFILE (
-	MEMBER_IDX 		INT 			NOT NULL 	COMMENT '회원 고유번호',
-	BUSINESS_NAME 	VARCHAR(255) 	NOT NULL 	COMMENT '상호명',
-	BUSINESS_URL 	TEXT 			NULL 	 	COMMENT '사업장 관련 URL',
-	PRIMARY KEY (MEMBER_IDX),
-	FOREIGN KEY (MEMBER_IDX) REFERENCES TB_MEMBER(MEMBER_IDX)
-    	ON DELETE CASCADE
-        ON UPDATE CASCADE
+-- 소상공인
+create table tb_owner_profile (
+	member_idx		int			not null comment '회원 고유번호',
+	business_name	varchar(255)	not null comment '상호명',
+	business_url	text		null comment '사업장 관련 url',
+	primary key (member_idx),
+	foreign key (member_idx) references tb_member(member_idx)
+		on delete cascade
+		on update cascade
 ) COMMENT='소상공인 프로필 테이블';
--- drop table TB_OWNER_PROFILE;
-
-
-
-
-
-
-
-
-
-
-
--- backend/src/main/webapp/DBsql/query_SQL.sql
+++ backend/src/main/webapp/DBsql/query_SQL.sql
@@ -1,22 +1,33 @@
+################## 각 테이블 조회 ##############
+SELECT * FROM tb_common_code;
+SELECT * FROM tb_member;
+SELECT * FROM tb_member_auth;
+SELECT * FROM tb_reviewer_profile;
+SELECT * FROM tb_reviewer_channel;
+SELECT * FROM tb_owner_profile;
+
+############## 공통 코드 조회 ###############
+select * from tb_common_code where del_yn='n' order by group_sort, sort asc;
+
 ################# 리뷰어 전체 조회 ##################
 select * 
-FROM TB_MEMBER MB JOIN TB_MEMBER_AUTH AU
-ON MB.MEMBER_IDX = AU.MEMBER_IDX
-JOIN TB_REVIEWER_PROFILE RP
-ON MB.MEMBER_IDX = RP.MEMBER_IDX;
+FROM tb_member mb JOIN tb_member_auth au
+ON mb.member_idx = au.member_idx
+JOIN tb_reviewer_profile rp
+ON mb.member_idx = rp.member_idx;
 
 ############ 리뷰어 채널 전체 조회 #############
-select MB.MEMBER_IDX, MB.MEMBER_NAME, RC.INF_TYPE_CODE_ID, RC.CHANNEL_URL
-FROM TB_MEMBER MB JOIN TB_MEMBER_AUTH AU
-ON MB.MEMBER_IDX = AU.MEMBER_IDX
-JOIN TB_REVIEWER_PROFILE RP
-ON MB.MEMBER_IDX = RP.MEMBER_IDX
-JOIN TB_REVIEWER_CHANNEL RC
-ON MB.MEMBER_IDX = RC.MEMBER_IDX;
-#################　소상공인　전체　조회 ####################
-select * 
-FROM TB_MEMBER MB JOIN TB_MEMBER_AUTH AU
-ON MB.MEMBER_IDX = AU.MEMBER_IDX
-JOIN TB_OWNER_PROFILE OP
-ON MB.MEMBER_IDX = OP.MEMBER_IDX;
+select mb.member_idx, mb.member_name, rc.inf_type_code_id, rc.channel_url
+FROM tb_member mb JOIN tb_member_auth au
+ON mb.member_idx = au.member_idx
+JOIN tb_reviewer_profile rp
+ON mb.member_idx = rp.member_idx
+JOIN tb_reviewer_channel rc
+ON mb.member_idx = rc.member_idx;
 
+################# 소상공인 전체 조회 ####################
+select * 
+FROM tb_member mb JOIN tb_member_auth au
+ON mb.member_idx = au.member_idx
+JOIN tb_owner_profile op
+ON mb.member_idx = op.member_idx;
