561cc41dfe5ffaf1f2f8f9f840b0bc47e8e12390
Bongjung Kim <apple75391@gmail.com>
2025-06-02 20:04:15 +0900

    2025-06-02 commonApi.js 누락 파일 추가
    - bongjung 2025-06-02 -

---
+++ frontend/capstone_react/src/api/commonApi.js
@@ -0,0 +1,17 @@
+// /src/api/commonApi.js
+
+export async function fetchCommonCode(groupCode) {
+    const res = await fetch(`/api/common/${groupCode}`,{
+      method:'GET',
+      headers:{
+        "Accept":"application/json"
+      }
+    });
+
+    if (!res.ok) {
+        const err = await res.json().catch(() => ({}));
+        throw new Error(err.message || '공통코드 불러오는는 데 실패했습니다.');
+    }
+
+    return res.json();
+}
