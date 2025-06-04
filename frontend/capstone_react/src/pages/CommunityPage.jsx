d16567af1af00b608efea119beba092907e4f78d
gg-uks <skarnr659876@gmail.com>
2025-06-04 20:35:20 +0900

    -0604-dev push-충동해결완료

--- frontend/capstone_react/src/pages/CommunityPage.jsx
+++ frontend/capstone_react/src/pages/CommunityPage.jsx
@@ -4,7 +4,8 @@ import { useSearchParams } from "react-router-dom";
 import { AppContext } from "../contexts/AppContext";
 import { fetchCommunityPosts } from "../api/communityApi";
 import CommuCateBtns from "../components/CommuCateBtns";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
+
 const categoryColorMap = {
   COMMU001: "#FDD835",
   COMMU002: "#4DB6AC",
@@ -36,7 +37,7 @@ export default function CommunityPage() {
 
   // 🔁 카테고리 변경 핸들러
   const onCategoryChange = (categoryCode) => {
-      const updatedParams = {
+    const updatedParams = {
       ...params,
       categoryId: categoryCode,
     };
@@ -60,103 +61,103 @@ export default function CommunityPage() {
 
     setError(null);
     fetchCommunityPosts(token, queryString)
-      .then((data) => setPosts(data))
-      .catch((err) => setError(err.message));
+        .then((data) => setPosts(data))
+        .catch((err) => setError(err.message));
   }, [searchParams, token]);
 
   // if (loading) return <p className="text-center py-8">로딩 중…</p>;
   if (error)
     return (
-      <p className="text-center py-8 text-red-500">
-        에러: {error}
-      </p>
+        <p className="text-center py-8 text-red-500">
+          에러: {error}
+        </p>
     );
 
   return (
-    <div className="bg-white text-gray-800 min-h-screen">
-      <div className="max-w-6xl mx-auto px-4 py-8">
-        {/* 헤더 */}
-        <div className="flex justify-between items-center mb-6">
-          <h1 className="text-2xl font-bold">
-            커뮤니티
-          </h1>
-          <button className="bg-blue-100 text-blue-600 px-4 py-2 rounded-lg font-semibold">
-            글 작성
-          </button>
-        </div>
-
-        {/* 검색창 */}
-        <div className="flex justify-between items-center mb-2 flex-wrap">
-          {/* 왼쪽: 카테고리 버튼 */}
-          <div className="flex items-center gap-2">
-            <CommuCateBtns 
-              selectedCategory={params.categoryId}
-              onCategoryChange={onCategoryChange}
-            />
+      <div className="bg-white text-gray-800 min-h-screen">
+        <div className="max-w-6xl mx-auto px-4 py-8">
+          {/* 헤더 */}
+          <div className="flex justify-between items-center mb-6">
+            <h1 className="text-2xl font-bold">
+              커뮤니티
+            </h1>
+            <button className="bg-blue-100 text-blue-600 px-4 py-2 rounded-lg font-semibold">
+              글 작성
+            </button>
           </div>
 
-          {/* 오른쪽: 구분 + 검색 */}
-          <div className="flex items-center gap-2">
-            <div className="relative">
-              <select
-                name="searchCondition"
-                value={params.searchCondition}
-                onChange={
-                  onChangeSearchInput
-                }
-                className="appearance-none border px-4 py-3 pr-10 rounded-lg text-sm transition w-[100px] bg-white focus:outline-none"
-              >
-                <option value="">
-                  전체
-                </option>
-                <option value="TITLE">
-                  제목
-                </option>
-                <option value="CONTENT">
-                  내용
-                </option>
-              </select>
+          {/* 검색창 */}
+          <div className="flex justify-between items-center mb-2 flex-wrap">
+            {/* 왼쪽: 카테고리 버튼 */}
+            <div className="flex items-center gap-2">
+              <CommuCateBtns
+                  selectedCategory={params.categoryId}
+                  onCategoryChange={onCategoryChange}
+              />
+            </div>
 
-              {/* 커스텀 화살표 아이콘 */}
-              <div className="pointer-events-none absolute inset-y-0 right-3 flex items-center text-gray-500">
-                <svg
-                  className="w-4 h-4"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  viewBox="0 0 24 24"  
+            {/* 오른쪽: 구분 + 검색 */}
+            <div className="flex items-center gap-2">
+              <div className="relative">
+                <select
+                    name="searchCondition"
+                    value={params.searchCondition}
+                    onChange={
+                      onChangeSearchInput
+                    }
+                    className="appearance-none border px-4 py-3 pr-10 rounded-lg text-sm transition w-[100px] bg-white focus:outline-none"
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M19 9l-7 7-7-7"
-                  />
-                </svg>
+                  <option value="">
+                    전체
+                  </option>
+                  <option value="TITLE">
+                    제목
+                  </option>
+                  <option value="CONTENT">
+                    내용
+                  </option>
+                </select>
+
+                {/* 커스텀 화살표 아이콘 */}
+                <div className="pointer-events-none absolute inset-y-0 right-3 flex items-center text-gray-500">
+                  <svg
+                      className="w-4 h-4"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      viewBox="0 0 24 24"
+                  >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M19 9l-7 7-7-7"
+                    />
+                  </svg>
+                </div>
+              </div>
+              <div className="relative w-80">
+                <input
+                    type="text"
+                    value={params.searchKeyword}
+                    name="searchKeyword"
+                    placeholder="검색어를 입력해주세요."
+                    onChange={onChangeSearchInput}
+                    onKeyDown={onKeyDown}
+                    className="border px-3 py-3 pr-10 rounded-lg text-sm w-full focus:outline-none transition placeholder:text-xs"
+                />
+                <button
+                    onClick={onSearch}
+                    className="absolute inset-y-0 right-2 flex items-center text-gray-500 hover:text-black text-lg"
+                >
+                  <i className="fa fa-search" aria-hidden="true"></i>
+                </button>
               </div>
             </div>
-            <div className="relative w-80">
-              <input
-                type="text"
-                value={params.searchKeyword}
-                name="searchKeyword"
-                placeholder="검색어를 입력해주세요."
-                onChange={onChangeSearchInput}
-                onKeyDown={onKeyDown}
-                className="border px-3 py-3 pr-10 rounded-lg text-sm w-full focus:outline-none transition placeholder:text-xs"
-              />
-              <button
-                onClick={onSearch}
-                className="absolute inset-y-0 right-2 flex items-center text-gray-500 hover:text-black text-lg"
-              >
-                <i className="fa fa-search" aria-hidden="true"></i>
-              </button>
-            </div>
           </div>
-        </div>
 
-        {/* 테이블 */}
-        <table className="w-full text-sm table-auto border-t">
-          <thead className="text-left border-b-2 border-t-2">
+          {/* 테이블 */}
+          <table className="w-full text-sm table-auto border-t">
+            <thead className="text-left border-b-2 border-t-2">
             <tr className="text-gray-500 h-[50px]">
               <th className="py-2 w-[50px] text-center">
                 카테고리
@@ -177,99 +178,103 @@ export default function CommunityPage() {
                 좋아요
               </th>
             </tr>
-          </thead>
-          <tbody>
+            </thead>
+            <tbody>
             <tr className="bg-red-50 hover:bg-gray-50 border-b h-[70px] text-[15px]">
-                <td className="py-2 font-bold  text-center">
+              <td className="py-2 font-bold  text-center">
                   <span className="font-semibold text-red-600 bg-red-200 px-2 py-1 rounded">
                     필독
                   </span>
-                </td>
-                <td className="pl-8">
-                  1:1 문의 방법 & 자주 묻는 질문
-                  <span className="text-red-500 ml-1">[10]</span>
-                </td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-              </tr>
+              </td>
+              <td className="pl-8">
+                1:1 문의 방법 & 자주 묻는 질문
+                <span className="text-red-500 ml-1">[10]</span>
+              </td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
             {posts.length === 0 ? (
-              <tr>
-                <td
-                  colSpan={6}
-                  className="text-center py-10 text-gray-500"
-                >
-                  검색 결과가 없습니다.
-                </td>
-              </tr>
-            ) : (
-              posts.map((post) => {
-                const categoryColor = categoryColorMap[post.categoryId];
-
-                return (
-                  <tr
-                    //key={post.communityIdx}
-                    className="hover:bg-gray-50 border-b h-[70px] text-[15px]"
+                <tr>
+                  <td
+                      colSpan={6}
+                      className="text-center py-10 text-gray-500"
                   >
-                    <td className="py-2 font-bold  text-center">
+                    검색 결과가 없습니다.
+                  </td>
+                </tr>
+            ) : (
+                posts.map((post) => {
+                  const categoryColor = categoryColorMap[post.categoryId];
+
+                  return (
+                      <tr
+                          key={post.communityIdx}
+                          className="hover:bg-gray-50 border-b h-[70px] text-[15px]"
+                      >
+                        <td className="py-2 font-bold  text-center">
                       <span
-                        className="px-2 py-1 rounded-full text-white "
-                        style={{
-                          color:
+                          className="px-2 py-1 rounded-full text-white "
+                          style={{
+                            color:
                             categoryColor,
-                        }}
+                          }}
                       >
                         {post.codeNm}
                       </span>
-                    </td>
-                    <td className="pl-8">
-                      {post.title}
-                      {/* <span className="text-red-500 ml-1">[10]</span> */}
-                    </td>
-                    <td>
+                        </td>
+                        <td className="pl-8"> 260번째줄 커뮤니티 페이지jsx
+                          <Link
+                              to={`/community/${post.communityIdx}`}
+                              className="hover:underline text-blue-600"
+                          >
+                            {post.title}
+                          </Link>
+                        </td>
+                        <td>
                       <span className="text-gray-800">
                         {
                           post.writerName
                         }
                       </span>
-                      {post.auth ===
-                        "ROLE_OWNER" && (
-                        <span className="ml-2 text-xs font-semibold text-white bg-cyan-400 px-[2px] rounded">
+                          {post.auth ===
+                              "ROLE_OWNER" && (
+                                  <span className="ml-2 text-xs font-semibold text-white bg-cyan-400 px-[2px] rounded">
                           소
                         </span>
-                      )}
-                      {post.auth ===
-                        "ROLE_USER" && (
-                        <span className="ml-2 text-xs font-semibold text-white bg-lime-500 px-[2px] rounded">
+                              )}
+                          {post.auth ===
+                              "ROLE_USER" && (
+                                  <span className="ml-2 text-xs font-semibold text-white bg-lime-500 px-[2px] rounded">
                           리
                         </span>
-                      )}
-                      {post.auth ===
-                        "ROLE_ADMIN" && (
-                        <span className="ml-2 text-xs font-semibold text-white bg-red-600 px-[2px] rounded">
+                              )}
+                          {post.auth ===
+                              "ROLE_ADMIN" && (
+                                  <span className="ml-2 text-xs font-semibold text-white bg-red-600 px-[2px] rounded">
                           관
                         </span>
-                      )}
-                    </td>
-                    <td className="text-center">
-                      {new Date(
-                        post.regDate
-                      ).toLocaleDateString()}
-                    </td>
-                    <td className="text-center">
-                      {post.viewCount}
-                    </td>
-                    <td className="text-center">
-                      0
-                    </td>
-                  </tr>
-                );
-              })
+                              )}
+                        </td>
+                        <td className="text-center">
+                          {new Date(
+                              post.regDate
+                          ).toLocaleDateString()}
+                        </td>
+                        <td className="text-center">
+                          {post.viewCount}
+                        </td>
+                        <td className="text-center">
+                          0
+                        </td>
+                      </tr>
+                  );
+                })
             )}
-          </tbody>
-        </table>
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
   );
 }
